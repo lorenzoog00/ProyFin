43139cefdfa4023c234fb29cd4711b99cfdafbd6
lorenzoog00 <lorenzo.orozco.garcia@gmail.com>
2021-05-10 14:52:23 -0500

    laberinto se imprime de manera correcta y la funcion de encontrar la Entrada tambien, arrojando coordenadas (y,x)

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LONG 20
+//#include "Entrada_Salida"
+//#include "Pasos"
+
+void Enter();
+
+int main(int argc, char *argv[])
+{
+  char Caso2[LONG],Caso3[LONG],Caso4[LONG],Caso5[LONG];
+
+  system(#include <stdio.h>
+	 #include <stdlib.h>
+	 #include <string.h>
+
+	 #define LONG 20
+	 //#include "Entrada_Salida"
+	 //#include "Pasos"
+
+	 void Enter();
+
+	 int main(int argc, char *argv[])
+	 {
+	   char Caso2[LONG],Caso3[LONG],Caso4[LONG],Caso5[LONG];
+
+	   system("clear");
+
+	   if(argc == 1) // o sea laberinto
+	     {
+	       printf("- - - - - USO DEL PROGRAMA - - - - - \n");
+	       printf("Este programa despliega un laberinto, el cual para ser inicializado  \
+se seguirán\n los siguientes comandos:\n");
+	       printf("laberinto <Entrada>: inicializará el archivo que contiene el mejor ca\
+mino.\n");
+	       printf("laberinto -pasos <entrada>:Desplegará todos los caminos posibles así \
+como cuántos pasos recorrió y cuántas veces encontró la saldia. \n");
+	       printf("\n ");
+
+	     }
+
+	   //  if(argc == 2 && strcmp(Caso2,argv[]) == 0)
+	   //Caso2();
+	 }
+
+	 void Enter()
+	 {
+	   char Tecla;
+
+	   printf("Presiona <enter> para continuar...\n");
+	   Tecla = getchar();
+	 }         "clear");
+
+  if(argc == 1) // o sea laberinto
+    {
+      printf("- - - - - USO DEL PROGRAMA - - - - - \n");
+      printf("Este programa despliega un laberinto, el cual para ser inicializado  \
+se seguirán\n los siguientes comandos:\n");
+      printf("laberinto <Entrada>: inicializará el archivo que contiene el mejor ca\
+mino.\n");
+      printf("laberinto -pasos <entrada>:Desplegará todos los caminos posibles así \
+como cuántos pasos recorrió y cuántas veces encontró la saldia. \n");
+      printf("\n ");
+
+    }
+
+  //  if(argc == 2 && strcmp(Caso2,argv[]) == 0)
+  //Caso2();
+}
+
+void Enter()
+{
+  char Tecla;
+
+  printf("Presiona <enter> para continuar...\n");
+  Tecla = getchar();
+}        
